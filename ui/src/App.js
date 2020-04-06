6c427c75a75ff5f17b0f3cd47130efa60b890ba0
gorolykmaxim <streetbattle52@gmail.com>
2020-04-06 18:36:09 +0300

    ui: implement text edit

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 import Button from "./components/Button";
+import TextEdit from "./components/TextEdit";
 
 export default function App() {
   return (
-    <Button text={"learn more"} onClick={() => alert('kekw')}/>
+      <div>
+        <Button text={"learn more"} onClick={() => alert('kekw')}/>
+        <br/>
+        <TextEdit name={'kekw'} placeholder={'KekW'} className={'kekw'}/>
+      </div>
   );
 }
---
+++ ui/src/components/TextEdit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TextEdit.css';
+
+export default function TextEdit(props) {
+    return <input className={'serio-text-edit ' + (props.className || '')}
+                  type={'text'}
+                  name={props.name}
+                  defaultValue={props.defaultValue}
+                  placeholder={props.placeholder}/>;
+}
