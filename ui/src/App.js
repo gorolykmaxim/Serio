3de0abcfad14fe205ff2a5619cbcae1e392c9fc6
gorolykmaxim <streetbattle52@gmail.com>
2020-04-07 13:08:48 +0300

    ui: implement list tile

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import TextEdit from "./components/TextEdit";
 import TextArea from "./components/TextArea";
 import {BODY_1, Text} from "./components/Text";
+import ListTile from "./components/ListTile";
 
 export default function App() {
   return (
@@ -15,6 +16,10 @@ export default function App() {
           <TextArea name={'pepega'} placeholder={"Kekw\nMonkaOmega"}/>
           <br/>
           <Text type={BODY_1} primary>Kekw OmegaLUL</Text>
+          <br/>
+          <ListTile primaryText={'Kekw 1'} secondaryText={'OmegaLUL 1'}/>
+          <ListTile primaryText={'Kekw 2'} secondaryText={'OmegaLUL 2'} icon='visibility' onClick={() => alert('pepega')}/>
+          <ListTile primaryText={'Kekw 3'} secondaryText={'OmegaLUL 3'}/>
       </div>
   );
 }
---
+++ ui/src/components/ListTile.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './ListTile.css';
+import {BODY_1, BODY_2, Text} from "./Text";
+
+export default function ListTile(props) {
+    let icon;
+    if (props.icon) {
+        icon = <span className="material-icons">{props.icon}</span>;
+    } else {
+        icon = <span className="serio-list-tile-icon-placeholder"/>;
+    }
+    return (
+        <div className={'serio-list-tile'} tabIndex={0} onClick={props.onClick}>
+            {icon}
+            <div className={'serio-list-tile-content'}>
+                <Text type={BODY_1}>{props.primaryText}</Text>
+                <Text type={BODY_2}>{props.secondaryText}</Text>
+            </div>
+        </div>
+    );
+}
