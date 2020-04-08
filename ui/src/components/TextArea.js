63b2ccde11b9f565540fffc925ffe3b45acc84e2
gorolykmaxim <streetbattle52@gmail.com>
2020-04-08 16:21:35 +0300

    ui: allow making TextArea required in forms

--- ui/src/components/TextArea.js
+++ ui/src/components/TextArea.js
@@ -4,6 +4,7 @@ import './TextArea.css';
 export default function TextArea(props) {
     return (
         <textarea className={`serio-text-area ${props.className || ''}`}
+                  required={props.required}
                   name={props.name}
                   placeholder={props.placeholder}
                   autoFocus={props.autoFocus}>
