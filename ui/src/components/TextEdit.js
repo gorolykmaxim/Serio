0e20e7551def3844bede0ff075bf9eefea85dbb7
gorolykmaxim <streetbattle52@gmail.com>
2020-04-08 15:56:29 +0300

    ui: allow making TextEdit required in forms

--- ui/src/components/TextEdit.js
+++ ui/src/components/TextEdit.js
@@ -4,6 +4,7 @@ import './TextEdit.css';
 export default function TextEdit(props) {
     return <input className={`serio-text-edit ${props.className || ''}`}
                   type={'text'}
+                  required={props.required}
                   name={props.name}
                   defaultValue={props.defaultValue}
                   disabled={props.disabled || false}
