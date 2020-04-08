e4ddd4f9a4a358f9a36194c60fadce68df46eace
gorolykmaxim <streetbattle52@gmail.com>
2020-04-08 15:56:08 +0300

    ui: allow using Button as a submit button in forms

--- ui/src/components/Button.js
+++ ui/src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Button.css';
 
 export default function Button(props) {
+    const isSubmit = props.submit || false;
     const isDisabled = props.disabled || false;
     const className = ['serio-button', 'serio-text-font'];
     if (props.className) {
@@ -16,6 +17,7 @@ export default function Button(props) {
         <button className={className.join(' ')}
                 onClick={props.onClick}
                 disabled={isDisabled}
+                type={isSubmit ? 'submit' : 'button'}
                 autoFocus={props.autoFocus}>
             {props.text}
         </button>
