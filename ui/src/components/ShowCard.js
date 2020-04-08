70fde7526143b0695d729b8fbc08a8343b5bcc35
gorolykmaxim <streetbattle52@gmail.com>
2020-04-08 12:24:08 +0300

    ui: make hoverability of ShowCard configurable
    Specify hoverable=true when creating a ShowCard if you want
    it to be hoverable. By default a ShowCard is non-hoverable.

--- ui/src/components/ShowCard.js
+++ ui/src/components/ShowCard.js
@@ -6,8 +6,15 @@ export default function ShowCard(props) {
     const {name, thumbnail, lastWatched} = props.show;
     const backgroundStyle = {backgroundImage: `url(${thumbnail})`};
     const onClick = props.onClick || (() => {});
+    const className = ['serio-show-card'];
+    if (props.hoverable) {
+        className.push('serio-show-card-hoverable');
+    }
+    if (props.className) {
+        className.push(props.className);
+    }
     return (
-        <button className={`serio-show-card ${props.className || ''}`}
+        <button className={className.join(' ')}
                 style={backgroundStyle}
                 autoFocus={props.autoFocus}
                 onClick={() => onClick(props.show)}>
