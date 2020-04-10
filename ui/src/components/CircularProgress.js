6fcae99c6d383692189f87605d0e1b81e237639f
gorolykmaxim <streetbattle52@gmail.com>
2020-04-10 10:25:17 +0300

    ui: implement CircularProgress component

---
+++ ui/src/components/CircularProgress.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './CircularProgress.css';
+
+export default function CircularProgress(props) {
+    return <div className="serio-circular-progress mdl-spinner mdl-js-spinner is-active"/>;
+}
