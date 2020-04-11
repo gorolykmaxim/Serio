ef0fa525fcd763c4f37664cfb4ce7c78bceef795
gorolykmaxim <streetbattle52@gmail.com>
2020-04-11 18:55:14 +0300

    ui: improve IconButton
    Increase variaty of available sizes.
    Adjust existing sizes.
    Allow making IconButton disabled.

--- ui/src/components/IconButton.js
+++ ui/src/components/IconButton.js
@@ -1,10 +1,33 @@
 import React from "react";
 import './IconButton.css';
 
-export default function IconButton(props) {
+export const MEDIUM_SIZE = 0;
+export const LARGE_SIZE = 1;
+
+export function IconButton(props) {
+    const iconClassName = ['material-icons'];
+    if (props.size === LARGE_SIZE) {
+        iconClassName.push('serio-icon-button-large-size');
+    } else if (props.size === MEDIUM_SIZE) {
+        iconClassName.push('serio-icon-button-medium-size');
+    } else {
+        iconClassName.push('serio-icon-button-normal-size');
+    }
+    const buttonClassName = ['serio-icon-button'];
+    if (props.isDisabled) {
+        buttonClassName.push('serio-icon-button-disabled');
+    } else {
+        buttonClassName.push('serio-icon-button-enabled')
+    }
+    if (props.className) {
+        buttonClassName.push(props.className);
+    }
     return (
-        <button className={`serio-icon-button ${props.className || ''}`} onClick={props.onClick} autoFocus={props.autoFocus} >
-            <span className={`material-icons ${props.large ? 'serio-icon-button-large-size' : 'serio-icon-button-normal-size'}`}>{props.icon}</span>
+        <button disabled={props.isDisabled}
+                className={buttonClassName.join(' ')}
+                onClick={props.onClick}
+                autoFocus={props.autoFocus}>
+            <span className={iconClassName.join(' ')}>{props.icon}</span>
         </button>
     );
 }
