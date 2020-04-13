66bb0a9f61d20db330c1bad5da127644c0076bd1
gorolykmaxim <streetbattle52@gmail.com>
2020-04-13 11:52:34 +0300

    ui: use a proper library to parse location hash

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import {SerioApplication} from "./SerioApplication";
 import SerioController from "./SerioController";
 import enableDevelopmentApi from "./DevelopmentApi";
+import {parse} from "query-string";
 
 const DESKTOP_PLATFORM = 0;
 const MOBILE_PLATFORM = 1;
@@ -12,10 +13,9 @@ const TV_PLATFORM = 2;
 const PRODUCTION_RUNTIME = 0;
 const DEVELOPMENT_RUNTIME = 1;
 
-let hash = window.location.hash;
-hash = hash ? hash.substr(1, hash.length - 1) : "01";
-window.platform = parseInt(hash[0]);
-window.runtimeType = parseInt(hash[1]);
+const hash = parse(window.location.hash);
+window.platform = parseInt(hash.platform || 0);
+window.runtimeType = parseInt(hash.runtimeType || 1);
 
 if (window.runtimeType === DEVELOPMENT_RUNTIME) {
     window.serioController = new SerioController();
