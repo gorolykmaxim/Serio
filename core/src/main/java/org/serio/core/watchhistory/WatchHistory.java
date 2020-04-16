67ec8779a57ab62f6a134d35d52a6679410c60ae
gorolykmaxim <streetbattle52@gmail.com>
2020-04-16 19:34:45 +0300

    core: add lookup of an individual show view by show's ID

--- core/src/main/java/org/serio/core/watchhistory/WatchHistory.java
+++ core/src/main/java/org/serio/core/watchhistory/WatchHistory.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface WatchHistory {
     List<ShowView> getShowWatchHistory();
+    ShowView getShowViewById(String showId);
     List<EpisodeView> getEpisodeWatchHistoryOfShow(String showId);
     void watchShowEpisode(String showId, String episodeId, Duration watchProgress);
     void clearWatchHistoryOfShow(String showId);
