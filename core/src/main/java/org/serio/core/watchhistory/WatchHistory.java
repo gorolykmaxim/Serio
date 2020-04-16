c81cebf62a87e7dd1b61d1a0fc4a2741766f820c
gorolykmaxim <streetbattle52@gmail.com>
2020-04-16 19:55:03 +0300

    core: improve WatchHistory's interface explicitness
    Instead of returning null, make
    WatchHistory.getShowViewById return Optional, stating
    that it is normal for a Show to not have a corresponding
    ShowView.

--- core/src/main/java/org/serio/core/watchhistory/WatchHistory.java
+++ core/src/main/java/org/serio/core/watchhistory/WatchHistory.java
@@ -2,10 +2,11 @@ package org.serio.core.watchhistory;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
 
 public interface WatchHistory {
     List<ShowView> getShowWatchHistory();
-    ShowView getShowViewById(String showId);
+    Optional<ShowView> getShowViewById(String showId);
     List<EpisodeView> getEpisodeWatchHistoryOfShow(String showId);
     void watchShowEpisode(String showId, String episodeId, Duration watchProgress);
     void clearWatchHistoryOfShow(String showId);
