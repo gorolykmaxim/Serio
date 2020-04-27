e2760e73af9c4215c37cc4dc6df9282e5c88fc4b
gorolykmaxim <streetbattle52@gmail.com>
2020-04-27 14:28:05 +0300

    core: create ApplicationController's dummy facade

---
+++ core/src/main/java/org/serio/core/applicationcontroller/ApplicationController.java
@@ -0,0 +1,139 @@
+package org.serio.core.applicationcontroller;
+
+import org.serio.core.clipboard.Clipboard;
+import org.serio.core.notifications.Notifications;
+import org.serio.core.showplayer.ShowPlayer;
+import org.serio.core.shows.Shows;
+import org.serio.core.showscrawler.ShowsCrawler;
+import org.serio.core.userinterface.UserInterface;
+
+public class ApplicationController {
+    private final Clipboard clipboard;
+    private final Notifications notifications;
+    private final ShowPlayer showPlayer;
+    private final Shows shows;
+    private final ShowsCrawler showsCrawler;
+    private final UserInterface userInterface;
+
+    public ApplicationController(Clipboard clipboard, Notifications notifications, ShowPlayer showPlayer, Shows shows,
+                                 ShowsCrawler showsCrawler, UserInterface userInterface) {
+        this.clipboard = clipboard;
+        this.notifications = notifications;
+        this.showPlayer = showPlayer;
+        this.shows = shows;
+        this.showsCrawler = showsCrawler;
+        this.userInterface = userInterface;
+    }
+
+    public void addShow() {
+
+    }
+
+    public void importShowFromJson() {
+
+    }
+
+    public void selectShow(String showId) {
+
+    }
+
+    public void editThumbnailCrawler() {
+
+    }
+
+    public void editEpisodeVideoCrawler() {
+
+    }
+
+    public void editEpisodeNameCrawler() {
+
+    }
+
+    public void back() {
+
+    }
+
+    public void saveShowCrawler(String showCrawlerName) {
+
+    }
+
+    public void importShowCrawler(String rawShowCrawler) {
+
+    }
+
+    public void previewCrawler(String rawCrawler) {
+
+    }
+
+    public void saveCrawler(String rawCrawler) {
+
+    }
+
+    public void viewCrawlLog(String showId) {
+
+    }
+
+    public void viewLogEntry(int logEntryId) {
+
+    }
+
+    public void playShow() {
+
+    }
+
+    public void playShowEpisode(int episodeNumber) {
+
+    }
+
+    public void editShowCrawler() {
+
+    }
+
+    public void shareShowCrawler() {
+
+    }
+
+    public void crawlShow() {
+
+    }
+
+    public void viewShowCrawlLog() {
+
+    }
+
+    public void initiateClearShowWatchHistory() {
+
+    }
+
+    public void initiateShowRemoval() {
+
+    }
+
+    public void reportShowWatchProgress(double progress) {
+
+    }
+
+    public void playPreviousEpisode() {
+
+    }
+
+    public void playNextEpisode() {
+
+    }
+
+    public void clearWatchHistory() {
+
+    }
+
+    public void deleteShow() {
+
+    }
+
+    public void playShowFromTheBeginning() {
+
+    }
+
+    public void viewAllShows() {
+
+    }
+}
