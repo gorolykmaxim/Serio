d1ca3efb1d3d48d06da55c8a92267c92b1e42d9d
gorolykmaxim <streetbattle52@gmail.com>
2020-04-30 16:59:24 +0300

    core: write JavaDocs for tasks, triggered from the crawl preview view

--- core/src/main/java/org/serio/core/applicationcontroller/tasks/crawlpreview/ViewCrawlLogTask.java
+++ core/src/main/java/org/serio/core/applicationcontroller/tasks/crawlpreview/ViewCrawlLogTask.java
@@ -6,7 +6,13 @@ import org.serio.core.applicationcontroller.event.EventStack;
 import org.serio.core.applicationcontroller.tasks.ControllerTask;
 import org.serio.core.userinterface.UserInterface;
 
+/**
+ * Displays the crawl log, produced while previewing the currently edited crawler.
+ */
 public class ViewCrawlLogTask implements ControllerTask {
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void execute(EventStack eventStack, UserInterface userInterface) {
         eventStack.peek(CrawlPreviewEvent.class).ifPresent(lastEvent -> {
