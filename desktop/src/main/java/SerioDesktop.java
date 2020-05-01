f0827307cb9fb9e22ebf7b488dcb2457857c1523
gorolykmaxim <streetbattle52@gmail.com>
2020-05-01 13:27:26 +0300

    desktop: create a POC of sending events to the WebView

--- desktop/src/main/java/SerioDesktop.java
+++ desktop/src/main/java/SerioDesktop.java
@@ -1,19 +1,33 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import javafx.application.Application;
+import javafx.concurrent.Worker;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import org.serio.core.applicationcontroller.event.ImportShowFromJsonEvent;
 
 public class SerioDesktop extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Serio");
         WebView webView = new WebView();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
         webView.getEngine().load(getClass().getResource("resources/assets/index.html").toString());
-        StackPane stackPane = new StackPane(webView);
-        Scene scene = new Scene(stackPane);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        webView.getEngine().getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED) {
+                try {
+                    primaryStage.show();
+                    webView.getEngine().executeScript("dispatchApplicationEvent(" + mapper.writeValueAsString(new ImportShowFromJsonEvent("lelw")) + ")");
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        primaryStage.setScene(new Scene(new StackPane(webView)));
     }
 
     public static void main(String[] args) {
