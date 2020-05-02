d7ca39805895ee253c6c22a53edb003b686de10f
gorolykmaxim <streetbattle52@gmail.com>
2020-05-02 19:12:50 +0300

    desktop: remove obsolete logging

--- desktop/src/main/java/org/serio/desktop/SerioDesktop.java
+++ desktop/src/main/java/org/serio/desktop/SerioDesktop.java
@@ -1,6 +1,5 @@
 package org.serio.desktop;
 
-import com.sun.javafx.webkit.WebConsoleListener;
 import javafx.application.Application;
 import javafx.concurrent.Worker;
 import javafx.scene.Scene;
@@ -38,9 +37,6 @@ public class SerioDesktop extends Application {
         Core core = new Core(null, null, null, storage, storage, storage, userInterface, storage);
         URL uiEntryPoint = getClass().getClassLoader().getResource("assets/index.html");
         webView.getEngine().load(uiEntryPoint.toString() + "#platform=0&runtimeType=0");
-        WebConsoleListener.setDefaultListener((w, message, lineNumber, sourceId) -> {
-            System.out.println(message + "[at " + lineNumber + "]");
-        });
         webView.getEngine().getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue == Worker.State.SUCCEEDED) {
                 primaryStage.show();
