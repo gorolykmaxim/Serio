3498117306aac2cb242d6e84d94aec41cbbd4836
gorolykmaxim <streetbattle52@gmail.com>
2020-05-04 15:10:00 +0300

    desktop: write JavaDocs for DesktopHttpClient

--- desktop/src/main/java/org/serio/desktop/DesktopHttpClient.java
+++ desktop/src/main/java/org/serio/desktop/DesktopHttpClient.java
@@ -12,21 +12,36 @@ import reactor.netty.tcp.TcpClient;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Desktop implementation of {@link HttpClient}.
+ *
+ * <p>Uses spring-webflux's {@link WebClient} as a transport. Has a default connection / read / write timeout
+ * set to {@link DesktopHttpClient#TIMEOUT} in milliseconds. Does not constraint the underlying connection
+ * pool's size.</p>
+ */
 public class DesktopHttpClient implements HttpClient {
+    public static final int TIMEOUT = 5000;
+
     private final WebClient webClient;
 
+    /**
+     * Construct an http client.
+     */
     public DesktopHttpClient() {
         TcpClient client = TcpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
                 .doOnConnected(connection -> {
-                    connection.addHandler(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
-                    connection.addHandler(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                    connection.addHandler(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                    connection.addHandler(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
                 });
         webClient = WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(reactor.netty.http.client.HttpClient.from(client)))
                 .build();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Future<String> fetchContentFromUrl(String url) {
         return webClient.method(HttpMethod.GET).uri(url).retrieve().bodyToMono(String.class).toFuture();
