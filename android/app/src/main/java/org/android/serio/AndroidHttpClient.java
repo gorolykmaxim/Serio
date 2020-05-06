365e78c850c1a59d6c3a2e631d8cf4c16b99d035
gorolykmaxim <streetbattle52@gmail.com>
2020-05-07 00:17:05 +0300

    android: write JavaDocs for AndroidHttpClient

--- android/app/src/main/java/org/android/serio/AndroidHttpClient.java
+++ android/app/src/main/java/org/android/serio/AndroidHttpClient.java
@@ -8,14 +8,25 @@ import org.serio.core.httpclient.HttpClient;
 
 import java.util.concurrent.Future;
 
+/**
+ * Android implementation of {@link HttpClient}.
+ *
+ * <p>Uses a non-blocking single-threaded event loop based client {@link AsyncHttpClient}.</p>
+ */
 public class AndroidHttpClient implements HttpClient {
     private static final int TIMEOUT = 5000;
     private final AsyncHttpClient httpClient;
 
+    /**
+     * Construct an http client.
+     */
     public AndroidHttpClient() {
         this.httpClient = AsyncHttpClient.getDefaultInstance();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Future<String> fetchContentFromUrl(String url) {
         AsyncHttpRequest request = new AsyncHttpGet(url);
