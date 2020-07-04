3738e455b2a9ca442015d5ef4347f9a115b039b1
gorolykmaxim <streetbattle52@gmail.com>
2020-07-04 14:00:35 +0300

    core: fix error message about an unknown crawler step type specified

--- core/src/main/java/org/serio/core/showscrawler/crawler/UnknownCrawlerStepTypeException.java
+++ core/src/main/java/org/serio/core/showscrawler/crawler/UnknownCrawlerStepTypeException.java
@@ -6,6 +6,6 @@ import java.util.Collection;
 
 public class UnknownCrawlerStepTypeException extends CrawlerStepTypeException {
     public UnknownCrawlerStepTypeException(String type, Collection<String> existingTypes) {
-        super(String.format("Unknown crawler step type '%s'. Supported types are: %s", type, StringUtils.join(", ", existingTypes)));
+        super(String.format("Unknown crawler step type '%s'. Supported types are: %s", type, StringUtils.join(existingTypes, ", ")));
     }
 }
