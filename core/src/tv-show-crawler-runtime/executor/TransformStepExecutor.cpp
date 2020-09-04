a7982a7fb10b8742e40e815ead6da8ae2574cf28
Maksym <goroliuk.maksym@gmail.com>
2020-09-04 09:54:02 +0300

    Fix "template" property lookup in TransformStepExecutor.cpp
    Lookup "template" property value outside the loop.

--- core/src/tv-show-crawler-runtime/executor/TransformStepExecutor.cpp
+++ core/src/tv-show-crawler-runtime/executor/TransformStepExecutor.cpp
@@ -6,8 +6,9 @@ std::vector<std::string> serio::core::TransformStepExecutor::execute(const serio
     std::vector<std::string> results;
     results.reserve(previousStepResults.size());
     std::regex regex("%s");
+    std::string template_ = step.getPropertyOrFail("template");
     for (const std::string& previousResult: previousStepResults) {
-        results.push_back(std::regex_replace(step.getPropertyOrFail("template"), regex, previousResult));
+        results.push_back(std::regex_replace(template_, regex, previousResult));
     }
     return results;
 }
