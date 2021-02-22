4caa39ca2026e42f40c73efbecc95c484af70506
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-02-22 11:37:22 +0200

    Allow to specify maxWidth of Content.js

--- ui/src/common/Content.js
+++ ui/src/common/Content.js
@@ -1,9 +1,15 @@
 import {Container, Grow} from "@material-ui/core";
 
+/**
+ * @param {{maxWidth: 'xs'|'sm'|'md'|'lg'|'xl'|boolean, classes: Object}} props
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Content(props) {
+    const maxWidth = props.maxWidth !== undefined ? props.maxWidth : "sm";
     return (
         <Grow in>
-            <Container maxWidth="sm">
+            <Container maxWidth={maxWidth} classes={props.classes}>
                 {props.children}
             </Container>
         </Grow>
