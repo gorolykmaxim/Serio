4a0416db3bd2c992bf88a4090849e9d7259359fb
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-03-25 23:57:50 +0200

    Interlink navigation between all the views in CoreStub
    Make pressing "back" in Search.js go to Settings.js.
    Make pressing "back" in Settings.js go to Search.js.
    Make pressing "search" in Search.js display list of
    tv shows.

--- ui/src/development/CoreStub.js
+++ ui/src/development/CoreStub.js
@@ -20,7 +20,7 @@ const settingsEvent = {
     viewId: 2,
     title: "Settings",
     backEvent: {
-        event: "back-to-settings"
+        event: "back-to-search"
     },
     settings: [
         {
@@ -74,29 +74,12 @@ const selectLanguageEvent = {
     }
 };
 
-const searchEvent = {
+const baseSearchEvent = {
     viewId: 5,
     searchText: "Search",
     searchString: "",
     selected: 0,
-    tvShows: [],
     noTvShowsFoundText: "No TV Shows Found :(",
-    // tvShows: Array(50).fill({
-    //     title: "Boruto very-very-very-very-very-very-very-very-very long title",
-    //     lastWatched: "2348723648723563278547132649124 days ago",
-    //     thumbnail: "https://thumbs.filmix.ac/posters/orig/boruto-2017_117605_0.jpg",
-    //     selectTvShowEvent: {
-    //         event: "select-tv-show",
-    //         id: "Boruto"
-    //     }
-    // }).concat(Array(50).fill({
-    //     title: "Boruto very-very-very-very-very-very-very-very-very long title",
-    //     thumbnail: "https://thumbs.filmix.ac/posters/orig/boruto-2017_117605_0.jpg",
-    //     selectTvShowEvent: {
-    //         event: "select-tv-show",
-    //         id: "Boruto"
-    //     }
-    // })),
     searchStringChangeEvent: {
         event: "search-string-change"
     },
@@ -104,9 +87,30 @@ const searchEvent = {
         event: "search"
     },
     backEvent: {
-        event: "back-to-search"
+        event: "back-to-settings"
     }
 };
+
+const searchEvent = Object.assign({tvShows: []}, baseSearchEvent);
+
+const searchEventWithTvShows = Object.assign({
+    tvShows: Array(50).fill({
+        title: "Boruto very-very-very-very-very-very-very-very-very long title",
+        lastWatched: "2348723648723563278547132649124 days ago",
+        thumbnail: "https://thumbs.filmix.ac/posters/orig/boruto-2017_117605_0.jpg",
+        selectTvShowEvent: {
+            event: "select-tv-show",
+            id: "Boruto"
+        }
+    }).concat(Array(50).fill({
+        title: "Boruto very-very-very-very-very-very-very-very-very long title",
+        thumbnail: "https://thumbs.filmix.ac/posters/orig/boruto-2017_117605_0.jpg",
+        selectTvShowEvent: {
+            event: "select-tv-show",
+            id: "Boruto"
+        }
+    })),
+}, baseSearchEvent);
 
 export class CoreStub {
     constructor() {
@@ -114,12 +118,13 @@ export class CoreStub {
         this.incomingToOutgoing[settingsEvent.settings[0].selectEvent.event] = setCrawlerConfigUrlEvent;
         this.incomingToOutgoing[settingsEvent.settings[1].selectEvent.event] = selectLanguageEvent;
         this.incomingToOutgoing[settingsEvent.settings[2].selectEvent.event] = clearCacheEvent;
-        this.incomingToOutgoing[settingsEvent.backEvent.event] = settingsEvent;
+        this.incomingToOutgoing[settingsEvent.backEvent.event] = searchEvent;
         this.incomingToOutgoing[setCrawlerConfigUrlEvent.saveEvent.event] = settingsEvent;
         this.incomingToOutgoing[setCrawlerConfigUrlEvent.valueChangeEvent.event] = setCrawlerConfigUrlEvent;
         this.incomingToOutgoing[selectLanguageEvent.selectEvent.event] = settingsEvent;
         this.incomingToOutgoing[clearCacheEvent.confirmEvent.event] = settingsEvent;
-        this.incomingToOutgoing[searchEvent.backEvent.event] = searchEvent;
+        this.incomingToOutgoing[searchEvent.searchEvent.event] = searchEventWithTvShows;
+        this.incomingToOutgoing[searchEvent.backEvent.event] = settingsEvent;
     }
     start() {
         setTimeout(() => {
