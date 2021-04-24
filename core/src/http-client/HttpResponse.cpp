ae16353ea8dcb64df518658980c7e332cef84f2e
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-04-24 14:54:27 +0300

    Get rid of existing implementation of core and crdb

--- core/src/http-client/HttpResponse.cpp
+++
@@ -1,9 +0,0 @@
-#include <http-client/HttpResponse.h>
-
-#include <utility>
-
-namespace serio {
-HttpResponse::HttpResponse(const std::string &response) : get([response] () {return response; }) {}
-
-HttpResponse::HttpResponse(std::function<std::string()>  get) : get(std::move(get)) {}
-}
