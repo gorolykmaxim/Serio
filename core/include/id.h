dfd8d9fda21242573d4d19c08da9da574f7fd154
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-06-13 14:01:53 +0300

    Get rid of redundant constructor in id_seed

--- core/include/id.h
+++ core/include/id.h
@@ -4,9 +4,7 @@
 typedef unsigned int id;
 
 struct id_seed {
-    id current_value;
-
-    id_seed();
+    id current_value = 1;
 };
 
 id create_id(id_seed& seed);
--- core/src/id.cpp
+++ core/src/id.cpp
@@ -1,7 +1,5 @@
 #include "id.h"
 
-id_seed::id_seed() : current_value(1) {}
-
 id create_id(id_seed &seed) {
     return seed.current_value++;
 }
