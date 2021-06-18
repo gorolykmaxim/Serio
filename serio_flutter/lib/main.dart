61d12a9ddde0fe2d95af2898078c909fea6290a8
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-06-18 13:23:52 +0300

    Implement title screen

--- serio_flutter/lib/main.dart
+++ serio_flutter/lib/main.dart
@@ -9,11 +9,27 @@ class HelloWorld extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Hello World",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello World"),
-        ),
-      )
+      theme: ThemeData(
+        canvasColor: Colors.black,
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.orange)
+        )
+      ),
+      home: TitleScreen()
+    );
+  }
+}
+
+class TitleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text(
+              "Serio",
+              style: Theme.of(context).textTheme.headline1.apply(fontFamily: "PassionOne")
+          )
+      ),
     );
   }
 }
