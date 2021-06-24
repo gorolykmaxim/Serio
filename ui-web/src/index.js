e133dbed0d43b62c5ef8c7f97d5016b22df92e80
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-06-24 20:45:29 +0300

    Implement EditTextDialog view
    Make simple version, that just displays stuff and
    does not do anything else. It is already able to
    render externally specified content though.
    Implement a way to request focus to a specific html
    element when the time is right. Doing it inside the
    create*() function is not such place, because
    all kinds of stuff can happen during it's call and
    actual displayElement invocation, like animations.
    Hell, the element is not even attached to DOM
    at that point, so focus() call will just get ignored.

--- ui-web/src/index.js
+++ ui-web/src/index.js
@@ -2,6 +2,15 @@ import * as bootstrap from "bootstrap";
 import "typeface-passion-one";
 import "./style.scss";
 
+const dummyContent = {
+    dialog: {
+        title: "Crawler config URL",
+        description: "Specify URL to the file, that contains configuration of all the crawlers, responsible for crawling tv shows.",
+        cancelText: "Cancel",
+        confirmText: "Save"
+    }
+};
+
 function create(tag, ...classes) {
     const e = document.createElement(tag);
     e.classList.add.apply(e.classList, classes);
@@ -9,11 +18,34 @@ function create(tag, ...classes) {
 }
 
 function createTitleScreen(ui) {
-    const root = create("div", "serio-title", "full-height");
-    const title = create("h1", "display-1", "not-selectable");
+    const root = create("div", "center-layout", "full-height");
+    const title = create("h1", "serio-title", "not-selectable", "text-primary");
     title.innerText = "Serio";
     root.appendChild(title);
     ui.displayNext = root;
+}
+
+function createEditTextDialog(ui, content) {
+    const root = create("div", "center-layout", "full-height");
+    const container = create("div", "center-content", "full-height", "smartphone-max-width");
+    const title = create("h3", "center-text", "not-selectable");
+    title.innerText = content.dialog.title;
+    container.appendChild(title);
+    const description = create("p", "text-muted", "not-selectable");
+    description.innerText = content.dialog.description;
+    container.appendChild(description);
+    const textField = create("input", "form-control");
+    textField.setAttribute("placeholder", "Crawler config URL");
+    container.appendChild(textField);
+    const save = create("button", "btn", "btn-primary", "mt-2");
+    save.innerText = content.dialog.confirmText;
+    container.appendChild(save);
+    const cancel = create("button", "btn", "btn-dark", "mt-2");
+    cancel.innerText = content.dialog.cancelText;
+    container.appendChild(cancel);
+    root.appendChild(container);
+    ui.displayNext = root;
+    ui.toFocus = textField;
 }
 
 function displayElement(ui) {
@@ -24,6 +56,7 @@ function displayElement(ui) {
     } else {
         ui.root.appendChild(ui.displayNext);
     }
+    ui.toFocus.focus();
     ui.currentView = ui.displayNext;
     ui.displayNext = null;
     setTimeout(() => {
@@ -37,4 +70,5 @@ const ui = {
     displayNext: null
 };
 createTitleScreen(ui);
+createEditTextDialog(ui, dummyContent);
 displayElement(ui);
