82c46bb4595f00080095d7015761c58aa507ed76
gorolykmaxim <goroliuk.maksym@gmail.com>
2021-07-15 10:39:01 +0300

    Add test for pressing back while being on title screen

--- core/test/functional-test.cpp
+++ core/test/functional-test.cpp
@@ -48,7 +48,10 @@ TEST_F(functional_test, open_first_time_enter_all_wrong_urls_then_enter_right_ur
     send_task(core, render_task["editText"]["valueChangedTask"], {CONFIG_URL});
     send_task(core, render_task["editText"]["saveValueTask"]);
     expect_equal(ENGLISH.downloading_crawler_config, get_render_task(core));
-    expect_equal(ENGLISH.title_screen, get_render_task(core));
+    render_task = get_render_task(core);
+    expect_equal(ENGLISH.title_screen, render_task);
+    // Press "Back" button while being on title screen.
+    EXPECT_FALSE(enqueue_back_task_from_render_task(render_task.dump(), core.task_queue));
 }
 
 // TODO: make following tests locale-parameterized (once config view is finished)
@@ -66,6 +69,14 @@ TEST_F(functional_test,
     init_core(core, DB_PATH);
     execute_all_core_tasks(core);
     expect_equal(ENGLISH.title_screen, get_render_task(core));
+}
+
+TEST_F(functional_test, should_exit_app_on_pressing_back_while_being_on_title_screen) {
+    pre_init_core_with_params(core, CONFIG_URL);
+    init_core(core, DB_PATH);
+    execute_all_core_tasks(core);
+    const auto render_task = *core.render_task_queue.try_dequeue();
+    EXPECT_FALSE(enqueue_back_task_from_render_task(render_task, core.task_queue));
 }
 // TODO: write additional tests for case when we start with config URL specified and request fails for all the possible
 // reasons (once config view is finished and test framework can init core with cache cleared).
